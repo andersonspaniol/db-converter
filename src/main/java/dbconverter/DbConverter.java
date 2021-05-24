a71df26920fdceeed6fe576d3f02321bdd19cb3d
andersonspaniol <anderson_spaniol@hotmail.com>
2021-05-23 21:01:36 -0300

    Created empty class DbConverter.

---
+++ src/main/java/dbconverter/DbConverter.java
@@ -0,0 +1,13 @@
+package dbconverter;
+
+/**
+ *
+ * @author Anderson
+ */
+public class DbConverter {
+    
+    public static void main(String[] args) {
+        System.out.println("Ok!");
+    }
+    
+}
