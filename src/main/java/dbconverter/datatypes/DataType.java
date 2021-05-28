23d3e439e99e27d20073ef4aebacfb539105c110
andersonspaniol <anderson_spaniol@hotmail.com>
2021-05-28 19:58:38 -0300

    Created the enum DataType to represent the data type stored in each column.

---
+++ src/main/java/dbconverter/datatypes/DataType.java
@@ -0,0 +1,17 @@
+package dbconverter.datatypes;
+
+/**
+ * Reresents the column datatype
+ *
+ * @author Anderson Spaniol
+ */
+public enum DataType {
+
+    CHAR,
+    VARCHAR,
+    DECIMAL,
+    INTEGER,
+    DATE,
+    BINARY;
+
+}
