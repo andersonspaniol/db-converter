58450e6a946b469328646fbc3d117dfdbbbcf877
andersonspaniol <anderson_spaniol@hotmail.com>
2021-06-02 21:04:13 -0300

    Created attribute columnValues to store the values of each column.

--- src/main/java/dbconverter/datatypes/TableRecord.java
+++ src/main/java/dbconverter/datatypes/TableRecord.java
@@ -1,10 +1,28 @@
 package dbconverter.datatypes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Class that represents a table record
- * 
+ *
  * @author Anderson Spaniol
  */
 public class TableRecord {
-    
+
+    private final Map<String, Object> columnValues;
+
+    public TableRecord() {
+        this.columnValues = new HashMap();
+    }
+
+    public void setColumnValue(String columnName, Object columnsValue) {
+        columnValues.put(columnName, columnsValue);
+    }
+
+    @Override
+    public String toString() {
+        return "TableRecord{" + "columnValues=" + columnValues + '}';
+    }
+
 }
